da782ade7215850e4efef256e460b7942e50da58
Steven <steven.dias@ynov.com>
2019-01-29 11:21:47 +0100

    update

--- SbouMorpion/SbouMorpion.py
+++ SbouMorpion/SbouMorpion.py
@@ -4,14 +4,14 @@ from tkinter import *
 can = Tk()
 
 
-def hello():
-    print("salut")
+def hello(i,s):
+    print(i,s)
 
 
 for i in range(3):
     for s in range(3):
-        Button(can, text="Quitter", command=hello).grid(row=i, column=s)
-    print(i)
+        Button(can, text="Quitter", command=hello(i, s)).grid(row=i, column=s)
+        
 
 
 
